75b6feabe54620900c54f8e9e793e261c882ec85
letthoung <thanhtung3496@gmail.com>
2020-07-17 10:11:08 -0400

    implemented QuoteAdapter and initialze adapter for the main activity

--- app/src/main/java/com/example/quickquotes/MainActivity.kt
+++ app/src/main/java/com/example/quickquotes/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quickquotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var layoutManager: LinearLayoutManager;
+    lateinit var adapter: QuoteAdapter;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutManager = LinearLayoutManager(this);
+        recyclerView.layoutManager = layoutManager;
+
+        adapter = QuoteAdapter();
+        recyclerView.adapter = adapter;
     }
 }
--- app/src/main/java/com/example/quickquotes/QuoteAdapter.kt
+++ app/src/main/java/com/example/quickquotes/QuoteAdapter.kt
@@ -1,23 +1,30 @@
 package com.example.quickquotes
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
 class QuoteAdapter : RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>() {
-    class QuoteViewHolder(v: View): RecyclerView.ViewHolder(v) {
-
+    class QuoteViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var view: View = v;
+        var quote: String = "";
+        fun bindQuote(quote: String){
+            this.quote = quote;
+            view.textView.text = quote;
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
-        TODO("Not yet implemented")
+        return QuoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row,parent,false));
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return 10;
     }
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindQuote("Sample quote");
     }
 }
